dccd94eaf6a223e6ea9248e2ef97c00523bdf2b9
TGuo <tonyguo2010@gmail.com>
2025-03-28 16:55:42 -0400

    init

---
+++ Info.js
@@ -0,0 +1,33 @@
+export default class Info{
+    constructor(){
+        this.account_address = "0xBeC1c873ad01AFa014320Ad21d47e579e09fd3E9"
+        this.private_key = "0xcdf338bef0b18b26f7645a7b5cf77d61ff2a63cc0f95178459d0229a1831576b"
+        this.trans_hash = "0xb52fb340935a7424e017d7791be06a9b78284dc3b9f4e306b5a1df9dec84cdb9"
+        this.contract_address = "0x7b8df794b807463f7302b8d8879c736745609bbf"
+        this.pub_url = "https://eth.llamarpc.com"
+        this.testnet_url = "https://holesky.drpc.org"
+    }
+
+    add_account(web3) {
+        web3.eth.accounts.wallet.add(this.private_key);
+    }
+
+    async output_chain_info(web3){
+        const gas = await web3.eth.getGasPrice();
+        const chainId = await web3.eth.getChainId();
+
+        console.log(`Chain ID is ${chainId}`);
+        console.log(`Gas price is ${gas}`);
+    }
+
+    async account_demo(web3){
+        const balance = await web3.eth.getBalance(this.account_address)
+        console.log(`balance = ${balance}`);
+    }
+    
+    async trans_demo(web3){
+        const trans = await web3.eth.getTransaction(this.trans_hash);
+        console.log(`Transaction From: ${trans.from}, To: ${trans.to}, Value: ${trans.value}`);
+        console.log(trans)
+    }
+}
---
+++ holesky.js
@@ -0,0 +1,42 @@
+
+import  Web3 from 'web3';
+import Info from './info.js'
+
+var info = new Info();
+// const rpcUrl = "https://ethereum-holesky-rpc.publicnode.com";
+// const web3 = new Web3(info.pub_url);
+
+const web3 = new Web3(info.testnet_url);
+
+// info.add_account(web3)
+// info.output_chain_info(web3)
+// info.account_demo(web3)
+// info.trans_demo(web3)
+
+
+
+/*
+
+const address = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984';
+
+// you can find the complete ABI on etherscan.io
+// https://etherscan.io/address/0x1f9840a85d5af5bf1d1762f925bdaddc4201f984#code
+const abi = [
+	{
+		name: 'symbol',
+		outputs: [{ type: 'string' }],
+		type: 'function',
+	},
+	{
+		name: 'totalSupply',
+		outputs: [{ type: 'uint256' }],
+		type: 'function',
+	}
+];
+
+// // instantiate the smart contract
+const uniswapToken = new web3.eth.Contract(abi, address);
+// const symbol = await uniswapToken.methods.symbol().call();
+
+// console.log('Uniswap symbol:', symbol);
+*/
