dfda9e332a67b85aea264eb46128637cdadb7776
tonyguo <tonyguo2010@gmail.com>
2025-03-31 23:26:08 -0400

    Contract

--- Info.js
+++ Info.js
@@ -3,6 +3,7 @@ export default class Info{
         this.account_address = "0xBeC1c873ad01AFa014320Ad21d47e579e09fd3E9"
         this.private_key = "0xcdf338bef0b18b26f7645a7b5cf77d61ff2a63cc0f95178459d0229a1831576b"
         this.trans_hash = "0xb52fb340935a7424e017d7791be06a9b78284dc3b9f4e306b5a1df9dec84cdb9"
+        // https://holesky.etherscan.io/address/0x7b8df794b807463f7302b8d8879c736745609bbf#code
         this.contract_address = "0x7b8df794b807463f7302b8d8879c736745609bbf"
         this.pub_url = "https://eth.llamarpc.com"
         this.testnet_url = "https://holesky.drpc.org"
@@ -29,5 +30,57 @@ export default class Info{
         const trans = await web3.eth.getTransaction(this.trans_hash);
         console.log(`Transaction From: ${trans.from}, To: ${trans.to}, Value: ${trans.value}`);
         console.log(trans)
+    }
+
+    async contract_demo(web3){
+        const abi = [
+            {
+                "inputs": [],
+                "name": "symbol",
+                "outputs": [
+                  {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+              },
+              {
+                "inputs": [],
+                "name": "name",
+                "outputs": [
+                  {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+              },
+              {
+                "inputs": [],
+                "name": "totalSupply",
+                "outputs": [
+                  {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                  }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+              },
+        ];
+
+        const contract = new web3.eth.Contract(abi, this.contract_address);
+        const symbol = await contract.methods.symbol().call();
+        console.log('Contract symbol:', symbol);
+        const total = await contract.methods.totalSupply().call();
+        console.log('Contract total supply:', total)
+        const name = await contract.methods.name().call();
+        console.log('Contract name is ', name)
     }
 }
